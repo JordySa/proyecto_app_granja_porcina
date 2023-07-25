6891027074b89c4eae4ee80f9596a9545f500be0
Jordy Sanipatin <jordy.sanipatin3188@utc.edu.ec>
2023-07-24 22:18:31 -0500

    controladores y archivos modificados, vrgas no me dañaran

---
+++ lib/views/reproduccion/list_reproduccion.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ListViewReproduccion extends StatefulWidget {
+  const ListViewReproduccion({super.key});
+
+  @override
+  State<ListViewReproduccion> createState() => _ListViewReproduccionState();
+}
+
+class _ListViewReproduccionState extends State<ListViewReproduccion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Reproduccion"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.add),
+            title: const Text("Agregar"),
+            subtitle: const Text("Agregar una nueva reproduccion"),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.pushNamed(context, "/reproduccion/add");
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.list),
+            title: const Text("Listar"),
+            subtitle: const Text("Listar todas las reproducciones"),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.pushNamed(context, "/reproduccion/list");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
